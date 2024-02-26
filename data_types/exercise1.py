8cfa1392ddb1f5dd762742c601d447e4b692885d
Bezayne Gabriel <gbezayne@gmail.com>
2024-02-26 12:52:50 -0600

    First book exercise

---
+++ data_types/exercise1.py
@@ -0,0 +1,12 @@
+print( """Data Types: \n
+'True'		 -> String \n
+False		 -> Boolean \n
+(1, 2, 3)	 -> Tuple \n 
+1.5		 -> Float \n 
+[1, 2, 3]	 -> List \n
+2		 -> Integer \n
+range(5)	 -> Range \n
+{1, 2, 3}	 -> Set \n
+None             -> NoteType \n
+{'foo': 'bar'}   -> Dictionary
+""")
